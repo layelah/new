6d1aac6df04d092647643945fb843d4f3fe6ade3
layelah <lah.comptepro@gmail.com>
2025-03-24 10:08:09 +0000

    Initial commit

---
+++ src/main/java/com/example/sb_car_2/SbCar2Application.java
@@ -0,0 +1,13 @@
+package com.example.sb_car_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCar2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCar2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sb_car_2/SbCar2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_car_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCar2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
